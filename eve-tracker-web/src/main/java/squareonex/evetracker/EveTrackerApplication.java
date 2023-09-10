7b7df929406bfdd889d4bc989a11812665ea8295
lukas_laptop <lukasreiche@outlook.de>
2023-09-10 13:17:27 +0200

    Initial commit

---
+++ eve-tracker-web/src/main/java/squareonex/evetracker/EveTrackerApplication.java
@@ -0,0 +1,12 @@
+package squareonex.evetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EveTrackerApplication.class, args);
+    }
+
+}
