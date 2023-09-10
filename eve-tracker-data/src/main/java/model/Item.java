c998511daaeb680f87e933c0d4516134eb6b6757
lukas_laptop <lukasreiche@outlook.de>
2023-09-10 14:31:30 +0200

    Created Item POJO

---
+++ eve-tracker-data/src/main/java/model/Item.java
@@ -0,0 +1,14 @@
+package model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Item {
+    @EqualsAndHashCode.Include
+    Long id;
+    String name;
+}
---
+++ eve-tracker-data/src/test/java/model/ItemTest.java
@@ -0,0 +1,25 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+    @Test
+    public void equalsShouldOnlyIncludeId(){
+        Item item1 = new Item();
+        item1.setId(0L);
+        item1.setName("name");
+
+        Item item2 = new Item();
+        item2.setId(0L);
+        item2.setName("name2");
+
+        assertEquals(item1, item2);
+
+        item2.setName("name");
+        item2.setId(1L);
+        assertNotEquals(item1, item2);
+    }
+
+}
