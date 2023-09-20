59b568b82cd24ce93abc027b5c70cf136e369229
lukas_laptop <lukasreiche@outlook.de>
2023-09-20 12:22:58 +0200

    Created UserService and map-based implementation

---
+++ eve-tracker-data/src/main/java/squareonex/evetrackerdata/services/UserService.java
@@ -0,0 +1,6 @@
+package squareonex.evetrackerdata.services;
+
+import squareonex.evetrackerdata.model.User;
+
+public interface UserService extends CrudService<User, Long>{
+}
---
+++ eve-tracker-data/src/main/java/squareonex/evetrackerdata/services/map/UserServiceImpl.java
@@ -0,0 +1,42 @@
+package squareonex.evetrackerdata.services.map;
+
+import squareonex.evetrackerdata.model.User;
+import squareonex.evetrackerdata.services.UserService;
+
+import java.util.Set;
+
+public class UserServiceImpl extends AbstractMapService<User, Long> implements UserService {
+    private Long key = 0L;
+
+    @Override
+    public Set<User> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public User findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public User save(User object) {
+        if (object.getId() == null){
+            while (map.containsKey(key))
+                key++;
+            object.setId(key);
+            return super.save(key, object);
+        }else {
+            return super.save(object.getId(), object);
+        }
+    }
+
+    @Override
+    public void delete(User object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
