7a46d18d1312d332ff3c1b51de98c128c80e42a9
lukas_laptop <lukasreiche@outlook.de>
2023-09-25 14:31:35 +0200

    Fixed missing persistence annotations for Job entity mentioned in #29

--- eve-tracker-data/src/main/java/squareonex/evetrackerdata/model/Job.java
+++ eve-tracker-data/src/main/java/squareonex/evetrackerdata/model/Job.java
@@ -1,18 +1,26 @@
 package squareonex.evetrackerdata.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NonNull;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "jobs")
 public class Job {
     @EqualsAndHashCode.Include
+    @Id
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
     @NonNull private Item product;
     @NonNull private Long quantity;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     @NonNull private User user;
     private LocalDateTime startedTime;
     private LocalDateTime finishedTime;
