9532b41714b2fced72586b70d2e73843dc9784bb
lukas_laptop <lukasreiche@outlook.de>
2023-09-25 16:43:36 +0200

    Refactored config to use separate profiles for map-based services (default) and their jpa implementations

--- eve-tracker-web/src/main/java/squareonex/evetracker/config/EveTrackerConfig.java
+++ eve-tracker-web/src/main/java/squareonex/evetracker/config/EveTrackerConfig.java
@@ -2,22 +2,45 @@ package squareonex.evetracker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import squareonex.evetrackerdata.repositories.*;
 import squareonex.evetrackerdata.services.*;
+import squareonex.evetrackerdata.services.datajpa.*;
 import squareonex.evetrackerdata.services.map.*;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "squareonex.evetrackerdata.repositories")
 public class EveTrackerConfig {
-    @Bean
-    ActivityService activityService(){
+    @Bean(name = "activityService")
+    @Profile({"default", "map"})
+    ActivityService activityMapService(){
         return new ActivityServiceMapImpl();
     }
-    @Bean
-    BlueprintService blueprintService(ActivityService activityService){
+    @Bean(name = "activityService")
+    @Profile("jpa")
+    ActivityService activityJPAService(ActivityRepository activityRepository) {
+        return new ActivityServiceImpl(activityRepository);
+    }
+    @Bean(name = "blueprintService")
+    @Profile({"default", "map"})
+    BlueprintService blueprintMapService(ActivityService activityService){
         return new BlueprintServiceMapImpl(activityService);
     }
-    @Bean
-    ProductService itemService(){
+    @Bean(name = "blueprintService")
+    @Profile("jpa")
+    BlueprintService blueprintJPAService(BlueprintRepository blueprintRepository) {
+        return new BlueprintServiceImpl(blueprintRepository);
+    }
+    @Bean(name = "itemService")
+    @Profile({"default", "map"})
+    ProductService itemMapService(){
         return new ProductServiceMapImpl();
+    }
+    @Bean(name = "itemService")
+    @Profile("jpa")
+    ProductService itemJPAService(ItemRepository itemRepository) {
+        return new ProductServiceImpl(itemRepository);
     }
     @Bean
     StorageService storageService() {
@@ -27,16 +50,34 @@ public class EveTrackerConfig {
     ProductionCostService productionCostService(StorageService storageService) {
         return new ProductionCostServiceImpl(storageService);
     }
-    @Bean
-    TransactionService transactionService(){
+    @Bean(name = "transactionService")
+    @Profile({"default", "map"})
+    TransactionService transactionMapService(){
         return new TransactionServiceMapImpl();
     }
-    @Bean
-    JobService jobService() {
-        return new JobServiceImpl();
+    @Bean(name = "transactionService")
+    @Profile("jpa")
+    TransactionService transactionJPAService(TransactionRepository transactionRepository) {
+        return new TransactionServiceImpl(transactionRepository);
     }
-    @Bean
-    UserService userService() {
-        return new UserServiceImpl();
+    @Bean(name = "jobService")
+    @Profile({"default", "map"})
+    JobService jobMapService() {
+        return new JobServiceMapImpl();
+    }
+    @Bean(name = "jobService")
+    @Profile("jpa")
+    JobService jobJPAService(JobRepository jobRepository) {
+        return new JobServiceImpl(jobRepository);
+    }
+    @Bean(name = "userService")
+    @Profile({"default", "map"})
+    UserService userMapService() {
+        return new UserServiceMapImpl();
+    }
+    @Bean(name = "userService")
+    @Profile("jpa")
+    UserService userJPAService(UserRepository userRepository) {
+        return new UserServiceImpl(userRepository);
     }
 }
