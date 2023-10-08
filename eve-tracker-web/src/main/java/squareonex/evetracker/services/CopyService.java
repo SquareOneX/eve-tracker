c60372745099d40f11b410ae6602b0a2417a7578
lukas_laptop <lukasreiche@outlook.de>
2023-10-08 16:50:49 +0200

    Created CopyService and its impl

---
+++ eve-tracker-web/src/main/java/squareonex/evetracker/services/CopyService.java
@@ -0,0 +1,7 @@
+package squareonex.evetracker.services;
+
+import squareonex.evetrackerdata.model.Blueprint;
+
+public interface CopyService {
+    Float calculateCostPerCopy(Blueprint blueprint);
+}
