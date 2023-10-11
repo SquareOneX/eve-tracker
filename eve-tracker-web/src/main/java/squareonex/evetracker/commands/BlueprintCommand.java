a8b5910037901a9b4935a752c35e8d5098ad60a1
Lukas <lukasreiche@outlook.de>
2023-10-11 18:37:40 +0200

    Fixed NullPointerExceptions by initializing collections in commands

--- eve-tracker-web/src/main/java/squareonex/evetracker/commands/BlueprintCommand.java
+++ eve-tracker-web/src/main/java/squareonex/evetracker/commands/BlueprintCommand.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Duration;
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -13,9 +14,37 @@ import java.util.Set;
 public class BlueprintCommand {
     private ActivityCommand activityCommand;
     private ItemCommand itemCommand;
-    private Set<BlueprintProductCommand> productCommands;
-    private Set<BlueprintMaterialCommand> materialCommands;
-    private Set<BlueprintCopyCommand> copyCommands;
-    private Set<BlueprintOriginalCommand> originalCommands;
+    private Set<BlueprintProductCommand> productCommands = new HashSet<>();
+    private Set<BlueprintMaterialCommand> materialCommands = new HashSet<>();
+    private Set<BlueprintCopyCommand> copyCommands = new HashSet<>();
+    private Set<BlueprintOriginalCommand> originalCommands = new HashSet<>();
     private Duration duration;
+
+    public void setProductCommands(Set<BlueprintProductCommand> productCommands) {
+        for (BlueprintProductCommand command : productCommands) {
+            command.setBlueprintCommand(this);
+        }
+        this.productCommands = productCommands;
+    }
+
+    public void setMaterialCommands(Set<BlueprintMaterialCommand> materialCommands) {
+        for (BlueprintMaterialCommand command : materialCommands) {
+            command.setBlueprintCommand(this);
+        }
+        this.materialCommands = materialCommands;
+    }
+
+    public void setCopyCommands(Set<BlueprintCopyCommand> copyCommands) {
+        for (BlueprintCopyCommand command : copyCommands) {
+            command.setBlueprintCommand(this);
+        }
+        this.copyCommands = copyCommands;
+    }
+
+    public void setOriginalCommands(Set<BlueprintOriginalCommand> originalCommands) {
+        for (BlueprintOriginalCommand command : originalCommands) {
+            command.setBlueprintCommand(this);
+        }
+        this.originalCommands = originalCommands;
+    }
 }
--- eve-tracker-web/src/main/java/squareonex/evetracker/commands/ItemCommand.java
+++ eve-tracker-web/src/main/java/squareonex/evetracker/commands/ItemCommand.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -13,6 +14,20 @@ public class ItemCommand {
     private Long id;
     private String name;
     private Boolean published;
-    private Set<BlueprintCommand> blueprints;
-    private Set<TransactionCommand> transactionCommand;
+    private Set<BlueprintProductCommand> blueprintCommands = new HashSet<>();
+    private Set<TransactionCommand> transactionCommands = new HashSet<>();
+
+    public void setBlueprintCommands(Set<BlueprintProductCommand> blueprintCommands) {
+        for (BlueprintProductCommand command : blueprintCommands) {
+            command.setProduct(this);
+        }
+        this.blueprintCommands = blueprintCommands;
+    }
+
+    public void setTransactionCommand(Set<TransactionCommand> transactionCommands) {
+        for (TransactionCommand command : transactionCommands) {
+            command.setItemCommand(this);
+        }
+        this.transactionCommands = transactionCommands;
+    }
 }
