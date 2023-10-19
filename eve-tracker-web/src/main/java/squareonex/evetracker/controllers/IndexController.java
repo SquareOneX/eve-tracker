1b94d5942155aeb522c388d5cd985086ba4a6c73
lukas_laptop <lukasreiche@outlook.de>
2023-10-19 16:00:31 +0200

    Added basic error page

--- eve-tracker-web/src/main/java/squareonex/evetracker/controllers/IndexController.java
+++ eve-tracker-web/src/main/java/squareonex/evetracker/controllers/IndexController.java
@@ -1,12 +1,19 @@
 package squareonex.evetracker.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class IndexController implements ErrorController {
     @RequestMapping({"", "index"})
     public String index(){
         return "index";
+    }
+
+    @RequestMapping("error")
+    public String handleError(HttpServletRequest request) {
+        return "error";
     }
 }
