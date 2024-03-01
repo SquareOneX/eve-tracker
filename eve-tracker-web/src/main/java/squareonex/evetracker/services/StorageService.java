d497a303625753e2c15263ad19b5aefc523f0c6a
Lukas <lukasreiche@outlook.de>
2024-03-01 09:05:38 +0100

    Added  documentation to public interfaces

--- eve-tracker-web/src/main/java/squareonex/evetracker/services/StorageService.java
+++ eve-tracker-web/src/main/java/squareonex/evetracker/services/StorageService.java
@@ -6,18 +6,34 @@ import java.util.Optional;
 
 public interface StorageService {
 
-    // Adds a specific quantity of an item to the storage
-    // Returns the new total quantity of this item in the storage
+    /**
+     * Add a specific quantity of an item to the storage
+     * @param item item to add to storage
+     * @param quantityToAdd quantity
+     * @return quantity in storage after success
+     */
     long add(Item item, long quantityToAdd);
 
-    // Removes a specific quantity of an item from the storage
-    // Returns the remaining quantity of this item in the storage, or an empty Optional if the item does not exist in the storage
+    /**
+     * Remove quantity of an item from the storage
+     * @param item item to remove from storage
+     * @param quantityToRemove quantity
+     * @return quantity in storage after success
+     */
     Optional<Long> remove(Item item, long quantityToRemove);
 
-    // Checks the quantity of an item in the storage
-    // Returns the quantity of this item in the storage, or an empty Optional if the item does not exist in the storage
+    /**
+     * Check the quantity of an item in the storage
+     * @param item item to check for
+     * @return quantity of this item in the storage
+     */
     Optional<Long> check(Item item);
-    // Checks if the storage contains the specified quantity of an item
-    // Returns true if the storage contains at least the specified quantity of the item, or false otherwise
+
+    /**
+     * Check if the storage contains the specified quantity of an item
+     * @param item item
+     * @param quantity quantity
+     * @return true if the storage contains at least the specified quantity of the item, or false otherwise
+     */
     boolean isAvailable(Item item, long quantity);
 }
